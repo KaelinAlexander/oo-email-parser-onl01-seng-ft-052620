a92eaa8a5987abc2523e4bdd9475f5a4a19e0e8c
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-08 16:30:25 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,9 +4,8 @@ class EmailAddressParser
 attr_accessor :email_list
 email_list = []
 
-def parse
-    @email_list << self.new
-    converted = @email_list.(/[," "]/, "*")
+def self.parse
+    converted = self.new.(/[," "]/, "*")
     string = converted.split("*")
     binding.pry
     cleaned = string.reject{|element| element.empty?}
