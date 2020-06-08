50d6804e0c16e0989a9ac162394885bc3fc595a3
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-08 16:24:45 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,6 +4,10 @@ class EmailAddressParser
 attr_accessor :email_list
 email_list = []
 
+def initialize
+  
+end
+
 def self.parse
     converted = self.gsub(/[," "]/, "*")
     string = converted.split("*")
