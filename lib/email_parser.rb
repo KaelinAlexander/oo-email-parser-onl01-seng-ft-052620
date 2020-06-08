6e7a5243f2c418cbfca7cd3f15bd281c0e29e533
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-08 16:14:40 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,10 +1,13 @@
+require "pry"
+
 class EmailAddressParser
 attr_accessor: email_list
 email_list = []
 
 def parser(list)
-    converted = list.gsub(/[.?!]/, "*")
+    converted = list.gsub(/[," "]/, "*")
     string = converted.split("*")
+    binding.pry
     cleaned = string.reject{|element| element.empty?}
     counted = cleaned.count
     counted
