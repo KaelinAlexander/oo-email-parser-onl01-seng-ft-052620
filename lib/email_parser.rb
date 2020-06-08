331aa24199916095d8585b38bb2dcd3d64d559c6
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-08 16:15:57 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,8 +4,8 @@ class EmailAddressParser
 attr_accessor :email_list
 email_list = []
 
-def parser(list)
-    converted = list.gsub(/[," "]/, "*")
+def parser(csv)
+    converted = csv.gsub(/[," "]/, "*")
     string = converted.split("*")
     binding.pry
     cleaned = string.reject{|element| element.empty?}
